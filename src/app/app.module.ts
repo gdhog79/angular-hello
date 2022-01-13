1ece3cf63358b883b3a4a09e85e2d6582f03ce0a
gdhog79 <gdhog79@yahoo.com>
2022-01-13 17:39:54 -0500

    happy!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrdersComponent } from './orders/orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomerComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule
